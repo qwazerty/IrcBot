919de71157d5ddccf744325cf09b1aefda43ac26
Kevin "qwazerty" Houdebert <precursor.qwaz@hotmail.fr>
2014-01-27 18:45:31 +0000

    Refactor Irc module

--- Irc.pm
+++ Irc.pm
@@ -15,13 +15,13 @@ $| = 1;
 sub new {
     my $class = shift;
     my $self = {
-        _host => shift,
-        _port => shift,
-        _proto => shift,
-        _nick => shift,
-        _pass => shift,
-        _chan => shift,
-        _socket => undef,
+        host => shift,
+        port => shift,
+        proto => shift,
+        nick => shift,
+        pass => shift,
+        chan => shift,
+        socket => undef,
     };
     bless $self, $class;
     return $self;
@@ -30,25 +30,26 @@ sub new {
 sub connect {
     my ($self, $host, $port, $proto) = @_;
     if ($host) {
-        $self->{_host} = $host;
+        $self->{host} = $host;
     }
     if ($port) {
-        $self->{_port} = $port;
+        $self->{port} = $port;
     }
     if ($proto) {
-        $self->{_proto} = $proto;
+        $self->{proto} = $proto;
     }
-    $self->{_socket} = new IO::Socket::INET (
-        PeerHost => $self->{_host},
-        PeerPort => $self->{_port},
-        Proto => $self->{_proto},
+    $self->{socket} = new IO::Socket::INET (
+        PeerHost => $self->{host},
+        PeerPort => $self->{port},
+        Proto => $self->{proto},
     ) or die "Could not create socket : $!\n";
 
-    print "Connected to $self->{_host} on port $self->{_port}.\n";
+    print "Connected to $self->{host} on port $self->{port}.\n";
     my $thread = threads->create(sub {
         while (1) {
+            $self->{socket}->send("PING\n");
+            print "[DEBUG] PING $self->{host}.\n";
             sleep (60);
-            $self->{_socket}->send("PING\n");
         }
     });
 }
@@ -56,38 +57,35 @@ sub connect {
 sub authenticate {
     my ($self, $nick, $pass) = @_;
     if ($nick) {
-        $self->{_nick} = $nick;
+        $self->{nick} = $nick;
     }
     if ($pass) {
-        $self->{_pass} = $pass;
+        $self->{pass} = $pass;
     }
-    if (defined $self->{_pass}) {
-        $self->{_socket}->send("PASS $self->{_pass}\n");
+    if (defined $self->{pass}) {
+        $self->{socket}->send("PASS $self->{pass}\n");
     }
-    $self->{_socket}->send("NICK $self->{_nick}\n");
+    $self->{socket}->send("NICK $self->{nick}\n");
 }
 
 sub join {
     my ($self, $chan) = @_;
     if ($chan) {
-        $self->{_chan} = $chan;
+        $self->{chan} = $chan;
     }
-    $self->{_socket}->send("JOIN $self->{_chan}\n");
-    print "Joined channel $self->{_chan}.\n";
+    $self->{socket}->send("JOIN $self->{chan}\n");
+    print "Joined channel $self->{chan}.\n";
 }
 
 sub run_listen {
     my ($self) = @_;
-    my $socket = $self->{_socket};
+    my $socket = $self->{socket};
     while (1)
     {
         my $data = <$socket>;
         if (defined $data) {
-            if ($data =~ /:(\w*)[^\s]* PRIVMSG $self->{_chan} :(.*)$/) {
+            if ($data =~ /:(\w*)[^\s]* PRIVMSG $self->{chan} :(.*)$/) {
                 print "[$1] $2\n";
-            }
-            else {
-                print "[DEBUG] $data";
             }
         }
     }
